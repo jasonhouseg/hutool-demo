84987d499f5a1e48b1d6d995bb63175325c12f97
zjh <2536816955@qq.com>
2019-01-15 17:28:28 +0800

    测试生成随机id，日期

--- src/test/java/com/hutool/demo/DemoApplicationTests.java
+++ src/test/java/com/hutool/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.hutool.demo;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.RandomUtil;
 import org.junit.Test;
@@ -16,6 +17,13 @@ public class DemoApplicationTests {
         System.out.println(RandomUtil.randomInt(6));
         System.out.println(IdUtil.randomUUID());
         System.out.println(IdUtil.simpleUUID());
+
+        //当前时间字符串，格式：yyyy-MM-dd HH:mm:ss
+        String now = DateUtil.now();
+        System.out.println(now);
+        //当前日期字符串，格式：yyyy-MM-dd
+        String today= DateUtil.today();
+        System.out.println(today);
     }
 
 }
